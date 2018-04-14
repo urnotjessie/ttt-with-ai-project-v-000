e0a32107a13148b84635329f84c8cf6737d57396
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-14 23:55:15 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -42,7 +42,7 @@ class Game
 
       if self.current_player == "X"
 
-      (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      (position_1 == "O" && position_2 == "O" && position_3 == "O") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
     end
   end
 
