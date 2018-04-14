5cbb37b59e3448404a08f8a219547f119792ade6
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-14 19:22:53 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -12,5 +12,10 @@ class Board
 
   def display
     puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+    puts "-----------"
   end
 end
