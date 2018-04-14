92d691ac9b9c0af2634cee44e703860cff073f1d
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-14 23:16:46 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -14,7 +14,7 @@ class Game
     [6, 4, 2]
   ]
 
-  def initialize(player_1 = Players::Human.new, player_2 = Players::Human.new, board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new, board = Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
