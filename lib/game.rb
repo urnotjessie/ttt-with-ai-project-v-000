fcdb465f7b7d17d443b64a32c23ad4d7679e5121
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-14 23:58:01 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -42,7 +42,9 @@ class Game
         position_3 = self.board.cells[win_index_3]
 
         position_1 == "O" && position_2 == "O" && position_3 == "O"
-      # elsif self.current_player == "O"
+      end
+
+      elsif self.current_player == "O"
       #   position_1 == "X" && position_2 == "X" && position_3 == "X"
       # end
     end
