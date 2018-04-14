3e23aa9caec131bf4a857beba35b9aef007b346b
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-14 19:55:46 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,8 +2,7 @@ module Players
   class Human < Player
 
     def move(board)
-    puts "Please input your position: "
-    user_input = gets.strip
+      puts "Please input your position: "
+      user_input = gets.strip
+    end
   end
-  end
-end
