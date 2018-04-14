22bf3fea4c94799fe5b9279365632ec3729a9148
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-14 23:47:26 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -42,7 +42,7 @@ class Game
         win_index_3 = win_combination[2]
 
         position_1 = self.board.cells[win_index_1]
-        position_2 = board[win_index_2]
+        position_2 = self.board.cells[win_index_2]
         position_3 = board[win_index_3]
 
         (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
