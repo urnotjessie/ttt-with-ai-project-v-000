3760d4a3e318ce21d6415bd7d3d16eb1039926e1
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-14 23:59:37 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -40,9 +40,9 @@ class Game
       position_2 = self.board.cells[win_index_2]
       position_3 = self.board.cells[win_index_3]
 
-      if self.current_player.tokend == "X"
+      if self.current_player.token == "X"
         position_1 == "O" && position_2 == "O" && position_3 == "O"
-      elsif self.current_player == "O"
+      elsif self.current_player.token == "O"
         position_1 == "X" && position_2 == "X" && position_3 == "X"
       end
     end
